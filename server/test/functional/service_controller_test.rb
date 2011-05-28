da2753ca734368bd9b26c12ee89a172d803edaf3
Fernando Barajas <fernyb@fernyb.net>
2011-05-27 17:52:21 -0700

    remove Rails as the server as we are now using Sinatra which is much cleaner and simpler

--- server/test/functional/service_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ServiceControllerTest < ActionController::TestCase
-  test "should get connect" do
-    get :connect
-    assert_response :success
-  end
-
-end
--- server/test/unit/helpers/service_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ServiceHelperTest < ActionView::TestCase
-end
