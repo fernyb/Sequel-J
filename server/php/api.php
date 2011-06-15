dff955798234e0e6d9c5efcb921e197d560b0c09
Joe Hoyle <joehoyle@gmail.com>
2011-06-15 19:17:58 +0100

    Added query and table_info to PHP api #14

--- server/php/api.php
+++ server/php/api.php
@@ -29,7 +29,19 @@ function sj_serve_api_request() {
 		case 'rows':
 			echo json_encode( sj_serve_endpoint_rows() );
 			exit;
-			
+		
+		case 'relations':
+			echo json_encode( sj_serve_endpoint_relations() );
+			exit;
+		
+		case 'table_info':
+			echo json_encode( sj_serve_endpoint_table_info() );
+			exit;
+		
+		case 'query':
+			echo json_encode( sj_serve_endpoint_query() );
+			exit;
+						
 	endswitch;
 
 }
@@ -100,6 +112,78 @@ function sj_serve_endpoint_rows() {
 	
 }
 
+function sj_serve_endpoint_relations() {
+	
+	$db = sj_connect_to_mysql_from_get();
+	
+	if( !$db || empty( $_GET['table'] ) )
+		return array( 'tables' => array(), 'error' => 'Could not connect to MySQL with credentials' );
+	
+	return array( 'relations' => array() );
+	
+}
+
+function sj_serve_endpoint_table_info() {
+	
+	$db = sj_connect_to_mysql_from_get();
+	
+	if( !$db || empty( $_GET['table'] ) )
+		return array( 'tables' => array(), 'error' => 'Could not connect to MySQL with credentials' );
+	
+	$table_info_sql = $db->get_row( "SHOW TABLE STATUS LIKE '{$_GET['table']}'" );
+	
+	foreach( $table_info_sql as $key => $value )
+		$table_info[strtolower($key)] = $value;
+		
+	//encodings
+	$encoding_sql = $db->get_results( "SELECT * FROM information_schema.character_sets ORDER BY character_set_name ASC" );
+	foreach( $encoding_sql as $key => $row )
+		$encoding[] = array( 'collation_name' => $row->CHARACTER_SET_NAME, 'collate_set_name' => $row->DEFAULT_COLLATE_NAME, 'description' => $row->DESCRIPTION );
+	
+	//collations
+	$collations_sql = $db->get_results( "SELECT * FROM information_schema.collations WHERE character_set_name = ( SELECT CHARACTER_SET_NAME FROM information_schema.character_sets WHERE DEFAULT_COLLATE_NAME = '{$table_info['collation']}' LIMIT 0, 1 ) ORDER BY 'collation_name' ASC" );
+	foreach( $collations_sql as $key => $row )
+		$collations[] = array( 'collation_name' => $row->COLLATION_NAME, 'collate_set_name' => $row->CHARACTER_SET_NAME, 'id' => $row->ID );
+	
+	$create_table = $db->get_row( "SHOW CREATE TABLE `{$_GET['table']}`" );
+	
+	return array( 
+		'status' 	=> $table_info, 
+		'engines' 	=> $db->get_col( "SELECT Engine FROM information_schema.engines WHERE support IN ('DEFAULT', 'YES')" ),
+		'encodings' => $encoding,
+		'collations'=>  $collations,
+		'sql'		=> end($create_table)
+	);
+	
+}
+
+function sj_serve_endpoint_query() {
+	
+	$db = sj_connect_to_mysql_from_get();
+	
+	if( !$db )
+		return array( 'tables' => array(), 'error' => 'Could not connect to MySQL with credentials' );
+		
+	$result = $db->get_results( $_GET['query'] );
+	
+	if( $db->last_error )
+		return array( 'query' => array(), 'error' => $db->last_error );
+		
+	$columns = array();
+	$rows = array();
+	foreach( $result as $row ) {
+	
+		$columns = array();
+	
+		foreach( $row as $col_name => $row_value ) 
+			$columns[] = $col_name;
+
+		$rows[] = array_values( (array) $row );
+	}
+	
+	return array( 'columns' => $columns, 'results' => $result );
+}
+
 /* Helper Functions */
 
 function sj_error_array( $error_message ) {
@@ -128,6 +212,8 @@ function sj_connect_to_mysql_from_get() {
 	
 	if( $credentials['database'] && !$db->select( $credentials['database'] ) )
 		return false;
-	
+
+	$db->hide_errors();
+
 	return $db;
 }
